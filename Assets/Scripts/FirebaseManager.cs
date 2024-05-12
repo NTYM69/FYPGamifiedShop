1557b28c348b822f790d3eec7489234c387041e7
NTYM69 <hessargnew10@gmail.com>
2024-05-13 00:03:16 +0800

    Added firebase database

--- Assets/Scripts/FirebaseManager.cs
+++ Assets/Scripts/FirebaseManager.cs
@@ -1,65 +1,65 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
 // using Firebase.Database;
-// using Firebase;
-// using Firebase.Auth;
-// using TMPro;
-// using System.Threading.Tasks;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+using System.Threading.Tasks;
 
-// public class FirebaseManager : MonoBehaviour
-// {
+public class FirebaseManager : MonoBehaviour
+{
     
-//     public TMP_Text ProfileUsername, TicketNo;
-//     DatabaseReference dbRef;
-//     Firebase.Auth.FirebaseAuth auth;
+    public TMP_Text ProfileUsername, TicketNo;
+    // DatabaseReference dbRef;
+    Firebase.Auth.FirebaseAuth auth;
 
-//     void Start() {
-//         auth = FirebaseAuth.DefaultInstance;
-//         dbRef = FirebaseDatabase.DefaultInstance.RootReference;
+    void Start() {
+        auth = FirebaseAuth.DefaultInstance;
+        // dbRef = FirebaseDatabase.DefaultInstance.RootReference;
         
-//         RetrieveUserData();
-//     }
+        // RetrieveUserData();
+    }
 
-//     public void RetrieveUserData() {
-//         Firebase.Auth.FirebaseUser currentUser = auth.CurrentUser;
-//         if (currentUser != null) {
-//             string userId = currentUser.UserId;
+    // public void RetrieveUserData() {
+    //     Firebase.Auth.FirebaseUser currentUser = auth.CurrentUser;
+    //     if (currentUser != null) {
+    //         string userId = currentUser.UserId;
 
-//             // Create a database reference for the user's data
-//             DatabaseReference userRef = dbRef.Child("Users").Child(userId);
+    //         // Create a database reference for the user's data
+    //         DatabaseReference userRef = dbRef.Child("Users").Child(userId);
 
-//             // Query playerQuery = dbRef.Child("Users").Child(userId);
+    //         // Query playerQuery = dbRef.Child("Users").Child(userId);
 
-//             // Attach a listener to the database reference to read the data
-//             userRef.GetValueAsync().ContinueWith(task =>
-//             {
-//                 if (task.IsFaulted)
-//                 {
-//                     Debug.LogError("Failed to retrieve user data: " + task.Exception);
-//                     return;
-//                 }
+    //         // Attach a listener to the database reference to read the data
+    //         userRef.GetValueAsync().ContinueWith(task =>
+    //         {
+    //             if (task.IsFaulted)
+    //             {
+    //                 Debug.LogError("Failed to retrieve user data: " + task.Exception);
+    //                 return;
+    //             }
 
-//                 // Parse the retrieved data
-//                 DataSnapshot dataSnapshot = task.Result;
-//                 if (dataSnapshot != null && dataSnapshot.Exists)
-//                 {
-//                     // Deserialize the JSON data into a C# object
-//                     GameData userData = JsonUtility.FromJson<GameData>(dataSnapshot.GetRawJsonValue());
+    //             // Parse the retrieved data
+    //             DataSnapshot dataSnapshot = task.Result;
+    //             if (dataSnapshot != null && dataSnapshot.Exists)
+    //             {
+    //                 // Deserialize the JSON data into a C# object
+    //                 GameData userData = JsonUtility.FromJson<GameData>(dataSnapshot.GetRawJsonValue());
 
-//                     ProfileUsername.text = userData.username;
-//                     TicketNo.text = userData.tickets.ToString();
+    //                 ProfileUsername.text = userData.username;
+    //                 TicketNo.text = userData.tickets.ToString();
 
-//                 }
-//                 else
-//                 {
-//                     Debug.LogWarning("User data does not exist.");
-//                 }
-//             });
-//         }
-//     }
-// }
+    //             }
+    //             else
+    //             {
+    //                 Debug.LogWarning("User data does not exist.");
+    //             }
+    //         });
+    //     }
+    // }
+}
 
    
 
